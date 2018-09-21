f0b24d2cf75abe6a88fabf771fca12877edfc09a
Meowth52 <meowth@meowth.se>
2018-09-21 15:27:12 +0200

    Started project
    And imported SQLite

---
+++ Recepticator/Recepticator/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Recepticator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SQLiteConnection.CreateFile("Test1.sqlite");
+            // create a new database connection:
+            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=Test1.sqlite;Version=3;");
+
+            // open the connection:
+            sqlite_conn.Open();
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+
+            // Let the SQLiteCommand object know our SQL-Query:
+            sqlite_cmd.CommandText = "CREATE TABLE test (id integer primary key, text varchar(100));";
+
+            // Now lets execute the SQL ;-)
+            sqlite_cmd.ExecuteNonQuery();
+        }
+    }
+}
